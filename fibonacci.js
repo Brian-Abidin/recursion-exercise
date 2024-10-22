b46c6540f07ce9b51a9a28d4814fcaa454b0803e
Brian Abidin <Brian.abidin1@gmail.com>
2024-10-22 12:09:30 -0400

    removed comments

--- fibonacci.js
+++ fibonacci.js
@@ -1,6 +1,4 @@
 function fibs(num) {
-  // returns array that contains num amount of numbers from the
-  // fibonacci sequence.
   const fibArr = [];
   for (let i = 0; i < num; i += 1) {
     if (i < 2) {
@@ -12,10 +10,7 @@ function fibs(num) {
   return fibArr;
 }
 
-console.log(fibs(8));
-
 function fibsRec(num) {
-  // does the same thing as fibs, however, recursively.
   console.log("recursive");
   if (num === 2) {
     return [0, 1];
@@ -25,27 +20,3 @@ function fibsRec(num) {
   fibArr.push(fibArr[num - 3] + fibArr[num - 2]);
   return fibArr;
 }
-const newarr = fibsRec(8);
-console.log(newarr);
-
-// fibsRec(8)
-// fibArr = fibsRec(7)
-// fibsRec(7)
-// fibsRec(6)
-// fibsRec(5)
-// fibsRec(4)
-
-// fibsRec(3)
-
-/* fibArr = [0, 1] because when num = 3, fibArr = fibsRec(2) which = [0, 1]
- *  then fibArr.push((fibArr[1] + fibArr[0]))
- * fibArr[1] + fibArr[0] = 1 + 0 because
- * fibArr = [0, 1] so, fibArr.push(1)
- * returns fibArr which is now [0, 1, 1]
- * so fibsRec(3) = [0, 1, 1]
- */
-// fibsRec(2) returns because num === 2 [0, 1]
-
-// create the fibonacci sequence then push it into the array
-// formula for the fibonacci sequence n = (n-1) + (n-2) except for
-// the first two digits which will always be 0 and 1.
--- merge-sort.js
+++ merge-sort.js
@@ -1,32 +1,17 @@
 function mergeSort(array) {
-  // takes in an array and
-  // returns a sorted array
-  // using recursive merge sort
-  // methodology.
   if (array.length === 1) {
     return array;
   }
   const half = (array.length / 2).toFixed(0);
   const leftArr = mergeSort(array.slice(0, half));
-  const rightArr = mergeSort(array.slice(half, array.length))
-  const sortedArr = []
-  while(leftArr.length > 0 && rightArr.length > 0){
-    if(leftArr[0] < rightArr[0]){
-      sortedArr.push(leftArr.shift())
+  const rightArr = mergeSort(array.slice(half, array.length));
+  const sortedArr = [];
+  while (leftArr.length > 0 && rightArr.length > 0) {
+    if (leftArr[0] < rightArr[0]) {
+      sortedArr.push(leftArr.shift());
     } else {
-      sortedArr.push(rightArr.shift())
+      sortedArr.push(rightArr.shift());
     }
   }
-  console.log(sortedArr)
-  return [...sortedArr,...leftArr, ...rightArr]
+  return [...sortedArr, ...leftArr, ...rightArr];
 }
-const arr = [1, 2, 3, 4, 5, 6, 7];
-const half = (arr.length / 2).toFixed(0);
-const arrLeft = arr.slice(0, half);
-const arrRight = arr.slice(half, arr.length);
-console.log(arrLeft, arrRight);
-
-console.log(mergeSort([9, 2, 15, 7, 4, 21, 6, 4]));
-
-const arr1 = [1];
-const arr2 = [5];
